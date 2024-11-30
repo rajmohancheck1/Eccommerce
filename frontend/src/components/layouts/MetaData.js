e61505bb745f0e910382229c457e4376cc3c459d
Rajmohan <rajmohanking20@gmail.com>
2024-11-30 11:54:05 +0530

    Final update

--- frontend/src/components/layouts/MetaData.js
+++ frontend/src/components/layouts/MetaData.js
@@ -3,11 +3,7 @@ import { Helmet } from "react-helmet-async"
 export default function MetaData({title}) {
     return (
         <Helmet>
-<<<<<<< HEAD
-            <title>{`${title} - Rajcart`}</title>
-=======
             <title>{`${title} - JVLcart`}</title>
->>>>>>> fc5bcd4 (Initial code commit)
         </Helmet>
     )
 }
